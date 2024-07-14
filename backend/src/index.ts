c838a81fe0b377f34ca97b6e4e7a203e154a45e9
aleksandrholuenko <zerogormy@mail.ru>
2024-07-15 00:21:32 +0800

    init backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,10 @@ import express from 'express'
 
 const app = express()
 
-const port = process.env.PORT || 8080
+const port = Number(process.env.PORT) || 10000
 
 app.use(express.json())
 
 app.get('/', () => 'Hello World!')
 
-app.listen(port, () => console.log("API STARTS ON " + port))
+app.listen(port, '0.0.0.0', () => console.log("API STARTS ON " + port))
